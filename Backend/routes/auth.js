b998327992b1a1fd3cc950de5c0d8be7d110aebf
Samrat-Deula7 <samratdeula461@gmail.com>
2025-09-26 09:37:24 +0545

    adding salt and has

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt=require('bcryptjs');
+const JWT_SECRET='Thisisa%*code';
+var jwt = require("jsonwebtoken");
 
 // Create a User using:POST "/api/auth/createuser". Doesn't require Auth
 router.post(
@@ -13,33 +16,44 @@ router.post(
     }),
     body("email", "Enter a valid email").isEmail(),
   ],
-   async (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     // If error is empty is false then there is error so the if statement cathes the error.
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-     // Check whether the user with this email exists already
-    
-        
-     
-     let user=await User.findOne({email:req.body.email});
-     if(user){
-        return res.status(400).json({error:"Sorry this email already exists"})
-     }
-     user= await User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    })
+    // Check whether the user with this email exists already
 
-    //   .then((user) => res.json(user))
-    //   .catch((err) => {
-    //     console.log(err), res.json({ error: "Please enter an unique value" });
-    //   });
-    res.json({"great":"Data saved succesfully"})
-}
-  
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry this email already exists" });
+      }
+      const salt = await bcrypt.genSalt(10);
+      secPass = await bcrypt.hash(req.body.password, salt);
+
+      user = await User.create({
+        name: req.body.name,
+        password: secPass,
+        email: req.body.email,
+      });
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      // This gives the user the authtoken using which the token can be transformed back into the user.id .And because of the secret helps to detect if the token has been Tampered(changed)
+      const authtoken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ authtoken: authtoken });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Some error occured")
+    }
+  }
 );
 
 module.exports = router;
