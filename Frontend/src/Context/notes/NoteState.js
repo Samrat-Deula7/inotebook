0b40f8ca37e7906e39be5d0c75a7f78ca1b4303b
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-10 16:37:05 +0545

    Finished providing note but still need to use api for displaying the actual data

--- Frontend/src/Context/notes/NoteState.js
+++ Frontend/src/Context/notes/NoteState.js
@@ -2,10 +2,120 @@ import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState=(props)=>{
+    const notesInitial = [
+      {
+        _id: "68d93ec032ec73063a37e436",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:20.392Z",
+        __v: 0,
+      },
+      {
+        _id: "68d93ec132ec73063a37e438",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook second time hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:21.749Z",
+        __v: 0,
+      },
+      {
+        _id: "68d93ec032ec73063a37e436",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:20.392Z",
+        __v: 0,
+      },
+      {
+        _id: "68d93ec132ec73063a37e438",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook second time hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:21.749Z",
+        __v: 0,
+      },
+      {
+        _id: "68d93ec032ec73063a37e436",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:20.392Z",
+        __v: 0,
+      },
+      {
+        _id: "68d93ec132ec73063a37e438",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook second time hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:21.749Z",
+        __v: 0,
+      },
+      {
+        _id: "68d93ec032ec73063a37e436",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:20.392Z",
+        __v: 0,
+      },
+      {
+        _id: "68d93ec132ec73063a37e438",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook second time hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:21.749Z",
+        __v: 0,
+      },
+      {
+        _id: "68d93ec132ec73063a37e438",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook second time hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:21.749Z",
+        __v: 0,
+      },
+      {
+        _id: "68d93ec132ec73063a37e438",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook second time hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:21.749Z",
+        __v: 0,
+      },
+      {
+        _id: "68d93ec132ec73063a37e438",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook second time hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:21.749Z",
+        __v: 0,
+      },
+      {
+        _id: "68d93ec132ec73063a37e438",
+        user: "68d65025f434de3915efaac8",
+        title: "Updating form new inotebook second time hatty potter",
+        description: "book is really great but it is hard to read",
+        tag: "personal",
+        date: "2025-09-28T13:57:21.749Z",
+        __v: 0,
+      },
+    ];
     
-    
+    const [notes, setNotes] = useState(notesInitial);
     return(
-        <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
+        <NoteContext.Provider value={{notes,setNotes}}>{props.children}</NoteContext.Provider>
     )
 }
 
