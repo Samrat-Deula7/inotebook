e050043423c1eaf878914a00eecbea27868d630b
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-10 13:27:20 +0545

    Completed some frontend

--- Frontend/src/Components/About.js
+++ Frontend/src/Components/About.js
@@ -1,16 +1,11 @@
 import React, { use, useEffect } from "react";
 import { useContext } from "react";
-import noteContext from "../Context/notes/noteContext";
 
 const About = () => {
-  const a = useContext(noteContext);
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, []);
+
   return (
     <div>
-      This is about {a.state.name} {a.state.class}
+      This is about 
     </div>
   );
 };
