13f7c8192a5c460e5601dade96868a071481f9b4
Samrat-Deula7 <samratdeula461@gmail.com>
2025-10-27 04:29:48 +0545

    minor changed made in db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebookDB";
+const mongoURL = "mongodb://localhost:27017/inotebookDB";
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(mongoURI);
+    await mongoose.connect(mongoURL);
     console.log("✅ Connected to Mongoose successfully");
   } catch (error) {
     console.error("❌ Mongoose connection failed:", error);
